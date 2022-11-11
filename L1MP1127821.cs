f0bc4ebfd643881af2d1ce781bff8ff6537ebc8a
ManuelPonce15 <111389145+ManuelPonce15@users.noreply.github.com>
2022-11-11 16:18:28 -0600

    Add files via upload

---
+++ L1MP1127821.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laboratorio_1_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre:");
+            String Nombre = Console.ReadLine();
+
+            /*COMENTARIOS*/
+
+            Console.Write("Hola Mundo");
+            Console.Write("Soy " + Nombre);
+            Console.ReadKey();
+        }
+    }
+}
